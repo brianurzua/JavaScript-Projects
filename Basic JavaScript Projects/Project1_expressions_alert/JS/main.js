579e45fcdab610ad543365c6f4833f34afdd4663
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-03-09 18:33:38 -0800

    Added all the code from the challenges assigned to us.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+// window.alert('Hello world!');
+
+// document.write('This is the document.write() method!')
+
+// var A = 'This is a string';
+// document.write(A);
+
+//String Assignment from step 49 in Javascript Course
+var x = 'This is variable X'; // 1. Assign a string value to a variable.
+window.alert(x); // 2. Use the window.alert() method to display the variable.
+
+//Escape Character from step 51 in Javascript course
+var y = "Brian then thought to himself \"This isn't it\" ";
+document.write(y.fontcolor("blue"));  //I made this text blue
+
+//Concatenation assignment from step 53 Javascript
+document.write('Hello' + ' World');
+
+var b = " Concatenated" + " String";
+document.write(b);
+
+//Variable Challenge from step 56 Javascript course
+
+var dog = "Koda", car = 'Chevy', drink = 'Coke';
+document.write(car + dog); //Concatenating variables together
+
+document.write(6+9) //This is an expression
