87a98a173afdc751de9798fd20361b1c81dd3220
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-03-09 21:41:58 -0800

    Code for PROJECT2_FUNCTIONS assignment

--- Basic JavaScript Projects/Project2_functions/js/main.js
+++ Basic JavaScript Projects/Project2_functions/js/main.js
@@ -1,8 +1,19 @@
+// These are the variables for function assignment on step 66 of Javascript course
 var x = 'Hello'
 document.write(x)
 var y = ' You!'
 document.write(y)
 
+
+// For element tiempo in HTML page
 function displayDate() {
 	document.getElementById('tiempo').innerHTML = Date();
+}
+
+
+// Using the += operator
+function writeSentence(){
+	var sentence = 'I am just ';
+	sentence += 'a rodeo clown!';
+	document.getElementById('rodeo').innerHTML = sentence;
 }
