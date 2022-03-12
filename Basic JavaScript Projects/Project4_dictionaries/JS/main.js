6a2137d9a42668a7ad49cfaf56363d5f690d82df
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-03-12 11:19:59 -0800

    Added code for dictionary

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,1 +1,11 @@
 //Dictionary
+function My_dict() {
+	var schoolCourses = {
+		Course:'Math',
+		Period:'2nd',
+		Term:'Fall',
+		Credits:4
+	};
+	delete schoolCourses.Course; // This will delete this value
+	document.getElementById('Dictionary').innerHTML = schoolCourses.Course //This will print the value
+}
