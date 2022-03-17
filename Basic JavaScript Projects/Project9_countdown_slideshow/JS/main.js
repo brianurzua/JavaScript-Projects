76943ed67b7daf70bc0e2b154b492d233e9b8957
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-03-17 15:11:13 -0700

    code for project 9 and slideshow

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,46 @@
+//Countdown function
+function countdown() {
+	var seconds = document.getElementById('seconds').value;  //This takes input from the input box
+	
+	function tick() {
+			seconds = seconds - 1;
+			timer.innerHTML = seconds;  //This displays the time on the screen.
+			var time = setTimeout(tick, 1000);
+			if (seconds == -1){  //When timer is finished and a second passes, this banner comes up.
+				alert("Time's up!");
+				clearTimeout(time);
+				timer.innerHTML = '';
+			}
+		}
+		tick();	//This calls the function.
+}
+
+
+
+// This code is for the slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
