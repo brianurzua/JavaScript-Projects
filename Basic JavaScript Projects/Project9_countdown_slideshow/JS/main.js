406a13c279e64dd7af7af33598479c70904a75f2
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-03-17 22:38:37 -0700

    Added comments to my code

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -19,14 +19,14 @@ function countdown() {
 
 // This code is for the slideshow
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex);  //Slides start at 1 
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n); //which slide do you wish to go
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideIndex = n);  //which slide are you on
 }
 
 function showSlides(n) {
@@ -35,10 +35,10 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) {  //For loop for slide
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++) { //For loop for dot 
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
