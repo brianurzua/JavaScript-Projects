47e60de1f56bd5c2ac48d15f16675ea1d2c6a396
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-04-05 23:42:43 -0700

    added comments and changed code

--- Basic JavaScript Projects/JS Challenges/JS Challenge js/hyperlink.js
+++ Basic JavaScript Projects/JS Challenges/JS Challenge js/hyperlink.js
@@ -1,12 +1,13 @@
 //Code for slideshow.
+  /*JQuery needs to be defined in script tag of html, or the code for the slideshow won't work.
+  you will get an error of 'Uncaught ReferenceError: jQuery is not defined' in console*/
+$("#slideshow > div:gt(0)").hide();
 
-jQuery("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-  jQuery('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  3000);
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
