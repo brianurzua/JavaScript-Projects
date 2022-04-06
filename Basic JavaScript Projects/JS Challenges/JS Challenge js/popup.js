d8b834364ae12c09669c522af0ef12555aebb876
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-04-05 22:30:40 -0700

    keyframe and popup code

--- Basic JavaScript Projects/JS Challenges/JS Challenge js/popup.js
+++ Basic JavaScript Projects/JS Challenges/JS Challenge js/popup.js
@@ -1,10 +1,14 @@
 // Functions for the popup form 
+
+//This function opens the popup
 function openForm() {
   document.getElementById("subForm").style.display = "flex";
 }
 
 
 //This closes the popup form.
+	//querySelector returns the first element that matches a CSS selector.
+		//addEventListener method attaches an event handler to a document.
 document.querySelector('.close').addEventListener("click", function() {
 	document.getElementById("subForm").style.display = "none";
 });
